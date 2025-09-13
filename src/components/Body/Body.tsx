f2df43b43373497d6b925f894caad04ce5f84bfd
gobong <gobongchoi2@hotmail.com>
2025-09-13 21:19:47 +0200

    first things

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import './Body.css'
+import SalatImg from '../../images/salat.png'
 
 function Body() {
   return (
     <div>
         <div className='Text'>
             Simple and Tasty Recipes
+        </div>
+
+        <div className='Box'>
+            <div className='Box-Card'>
+                <img src={SalatImg} alt='Salat' className='Salat-Img'/>
+                <div className='Title'>
+                    Fresh and Healthy Salat
+                </div>
+                
+            </div>
+            <div className='Box-Card'>
+                Fresh and Healthy Salat
+            </div>
 
         </div>
 
