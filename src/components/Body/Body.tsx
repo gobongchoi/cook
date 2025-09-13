986edddfc0e91982754de5731cb584265267af1e
gobong <gobongchoi2@hotmail.com>
2025-09-13 19:54:03 +0200

    first changes

---
+++ src/components/Body/Body.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+  return (
+    <div>
+        <div className='Text'>
+            Simple and Tasty Recipes
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Body
